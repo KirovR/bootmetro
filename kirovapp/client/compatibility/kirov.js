6fc0bef3a78bbc43239e7ca8bd32b4af22c89d45
Hristo Hristov <hristo.hristov@nevexis.com>
2015-03-16 11:33:45 +0200

    dynamic snow

--- kirovapp/client/compatibility/kirov.js
+++ kirovapp/client/compatibility/kirov.js
@@ -11,31 +11,39 @@ $(document).ready(function(){
 
       $('#pivot').pivot();
 
-           // set up hover panels
-           // although this can be done without JavaScript, we've attached these events
-          // because it causes the hover to be triggered when the element is tapped on a touch device
-        $('.hover').hover(function(){
-            $(this).addClass('flip');
-        },function(){
-            $(this).removeClass('flip');
-        });
+        //   // set up hover panels
+        //   // although this can be done without JavaScript, we've attached these events
+        //  // because it causes the hover to be triggered when the element is tapped on a touch device
+        //$('.hover').hover(function(){
+        //    $(this).addClass('flip');
+        //},function(){
+        //    $(this).removeClass('flip');
+        //});
 
+    //prevent right click code
 	window.oncontextmenu = function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
 	};
 
+    //live resize code
     $("#tile-listview-demo").css("height", $(window).height()-200);
-
     var globalTimer = null;
-
     $(window).resize(function() {
         clearTimeout(globalTimer);
         globalTimer = setTimeout(function(){$("#tile-listview-demo").css("height", $(window).height()-200)}, 500);
     });
 
+    //show code
+    var today = new Date();
+    var month = today.getMonth();
+    if(month == 11 || month == 0){ //only Dec and Jan
+        $.fn.snow();
+    }
 
-    $.fn.snow();
+    //web notifications code
+
+
 
 });
